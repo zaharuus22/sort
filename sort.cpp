4133160ab678b2756ebcb8ccebec80aea9fdc967
zaharuus22 <berlovzahar@gmail.com>
2026-03-07 18:41:36 +0700

    2
    считали

--- sort.cpp
+++ sort.cpp
@@ -5,11 +5,22 @@ std::ifstream ent("input.txt");
 std::ofstream esc("output.txt");
 
 
+int input(long long&n, long long*ms) {
+
+	ent >> n;
+	for (int i = 0; i < n; i++) {
+		ent >> ms[i];
+
+
+	}
+	return 0;
+}
+
 int main()
 {
-    esc << "Hello World!\n";
-
-
-
+	long long n = 0;
+	long long ms[1009];
+	input(n, ms);
+	return 0;
 }
 
